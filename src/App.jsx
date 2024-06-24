697c5a4740192b7b350c87d79643d3fbab99ca3e
Claudio Marino <claudio.marino@outlook.com>
2024-06-24 20:19:04 +0200

    created main files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Info from "/components/Info";
+import About from "/components/About";
+import Interest from "/components/Interest";
+
+export default function App() {
+	return (
+		<div>
+			<Info />
+			<About />
+			<Interest />
+		</div>
+	);
+}
