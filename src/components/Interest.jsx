74b765cd3460b618dce4c2f57bceac962cd7b48d
Claudio Marino <claudio.marino@outlook.com>
2024-06-25 13:49:24 +0200

    ok

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -3,7 +3,7 @@ export default function Interest() {
 		<div className="interest">
 			<h3 className="interest-title">Interests</h3>
 			<p className="interest-description">
-				Food expert. Music scholar. Reader. Internet fanatic. Bacon buff.
+				Food expert. Paddle player. Reader. Internet fanatic. Bacon buff.
 				Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
 			</p>
 		</div>
