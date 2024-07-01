43736e9bfae331b77fbb20abc09f0a1d5a40f92d
Claudio Marino <claudio.marino@outlook.com>
2024-07-01 19:39:36 +0200

    added instagram href

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,7 +12,10 @@ export default function Footer() {
 			<a href="" target="_blank" rel="noopener noreferrer">
 				<img src={facebook} alt="facebook icon" className="footer-icon" />
 			</a> */}
-			<a href="" target="_blank" rel="noopener noreferrer">
+			<a
+				href="https://www.instagram.com/marinodeveloper?igsh=eGRydXI0aDd4ZWgw"
+				target="_blank"
+				rel="noopener noreferrer">
 				<img src={instagram} alt="instagram icon" className="footer-icon" />
 			</a>
 			<a
