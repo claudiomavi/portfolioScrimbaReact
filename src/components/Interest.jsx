dad5cbe3ea36decc63b0f2e6c1378833cd7036c9
claudiomavi <118379948+claudiomavi@users.noreply.github.com>
2024-07-07 01:16:51 +0200

    Update Interest.jsx

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -5,12 +5,6 @@ export default function Interest() {
 			<p className="interest-description">
 				Jugador de pádel. Lector apasionado. Fanatico de la teconlogía.
 			</p>
-			<a
-				href="https://misproyectosdestacados.netlify.app/"
-				target="_blank"
-				rel="noopener noreferrer">
-				<p className="profile-web">👆 Mis proyectos destacados 👆</p>
-			</a>
 		</div>
 	);
 }
