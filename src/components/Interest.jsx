b1187b40b884895579ebeb56188debadfd1a0512
claudiomavi <118379948+claudiomavi@users.noreply.github.com>
2024-09-18 20:48:57 +0200

    Update Interest.jsx

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -3,7 +3,7 @@ export default function Interest() {
 		<div className="interest">
 			<h3 className="interest-title">Interests</h3>
 			<p className="interest-description">
-				Padel player. Passionate reader. Technology enthusiast.
+				React developer, technology enthusiast, and padel player. Always eager to learn and improve.
 			</p>
 		</div>
 	)
