a45207e86b879c448aeb03f3277ac008e3af6295
Claudio <claudio.marino@outlook.com>
2024-11-04 19:01:00 +0100

    ∑ok

--- src/components/Interest.jsx
+++ src/components/Interest.jsx
@@ -3,8 +3,14 @@ export default function Interest() {
 		<div className="interest">
 			<h3 className="interest-title">Interests</h3>
 			<p className="interest-description">
-				React developer, technology enthusiast, and padel player. Always eager to learn and improve.
+				Jugador de pádel. Lector apasionado. Fanatico de la tecnología.
 			</p>
+			<a
+				href="https://misproyectosdestacados.netlify.app/"
+				target="_blank"
+				rel="noopener noreferrer">
+				<p className="profile-web">👆 Mis proyectos destacados 👆</p>
+			</a>
 		</div>
 	)
 }
